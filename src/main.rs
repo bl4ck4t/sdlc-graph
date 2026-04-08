f17e1495bb829b7168b9eb9c3926830d22398f6a
Vinay Poojary <vp8448@gmail.com>
2026-04-08 19:13:37 +0530

    feat: implement axum server with root endpoint and structured logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener};
+
+use axum::{Router, routing::get};
+
+#[tokio::main]
+
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    
+    //Use following command to view logs in powershell
+    //$env:RUST_LOG="info"; cargo run 
+    tracing::info!("Server running on: {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app)
+        .await
+        .unwrap()
+}
+
+async fn root() -> &'static str {
+    "Hello, SDLC Graph!"
 }
